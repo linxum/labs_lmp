a21094daff5584c9edbddebf298a63331a5cd019
Ivan Smekchnev <49167389+linxum@users.noreply.github.com>
2023-05-24 10:42:33 +0300

    Update lab8.cpp

--- Lab 8/lab8.cpp
+++ Lab 8/lab8.cpp
@@ -16,7 +16,7 @@ bool isCovered(const std::vector<Point>& points, const std::vector<Line>& lines)
     for (const auto& point : points) {
         bool covered = false;
         for (const auto& line : lines) {
-            if (point.y == line.k * point.x + line.b) {
+            if (point.y == line.k * point.x + line.b || std::isnan(line.k)) {
                 covered = true;
                 break;
             }
@@ -28,7 +28,7 @@ bool isCovered(const std::vector<Point>& points, const std::vector<Line>& lines)
     return true;
 }
 
-void backtrack(const std::vector<Point>& points, std::vector<Line>& lines, std::vector<Line>& bestLines) {
+void backtrack(const std::vector<Point>& points, std::vector<Line>& lines, std::vector<Line>& bestLines, std::vector<bool>& used) {
     if (isCovered(points, lines)) {
         if (lines.size() < bestLines.size()) {
             bestLines = lines;
@@ -39,27 +39,44 @@ void backtrack(const std::vector<Point>& points, std::vector<Line>& lines, std::
         return;
     for (int i = 0; i < points.size(); i++) {
         for (int j = i + 1; j < points.size(); j++) {
-            if (points[i].x != points[j].x) 
+            if (!used[j])
             {
                 Line tmp;
-                tmp.k = (points[j].y - points[i].y) / (points[j].x - points[i].x);
-                tmp.b = points[i].y - tmp.k * points[i].x;
+                if (points[i].x == points[j].x)
+                {
+                    tmp.k = NAN;
+                    tmp.b = points[i].x;
+                }
+                else
+                {
+                    tmp.k = (points[j].y - points[i].y) / (points[j].x - points[i].x);
+                    tmp.b = points[i].y - tmp.k * points[i].x;
+                }
+                
+                used[j] = true;
                 lines.push_back(tmp);
-                backtrack(points, lines, bestLines);
+                backtrack(points, lines, bestLines, used);
                 lines.pop_back();
+                used[j] = false;
             }
         }
     }
 }
 
 int main() {
-    std::vector<Point> points = { {1, 1}, {1, 3}/*, {-1, 1}, {-1, -1}*/ };
+    std::vector<Point> points = { {1, 1}, {1, 7}/*, {7, 1}, {4, 4}*/ };
     std::vector<Line> lines;
     std::vector<Line> bestLines(points.size());
-    backtrack(points, lines, bestLines);
+    std::vector<bool> used(points.size(), false);
+    backtrack(points, lines, bestLines, used);
     std::cout << "Minimum set of lines: ";
     for (const auto& line : bestLines) {
-        std::cout << "y = " << line.k << "x + " << line.b << "; ";
+        if (std::isnan(line.k)) { // для вертикальных прямых выводим отдельное сообщение
+            std::cout << "x = " << line.b << "; ";
+        }
+        else {
+            std::cout << "y = " << line.k << "x + " << line.b << "; ";
+        }
     }
     std::cout << std::endl;
     return 0;
